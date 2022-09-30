38fbbcbfe6f2b22f5f79441c1365827d8086d408
RubenTipparach <santipab.stephane@gmail.com>
2022-09-30 18:59:26 -0500

    project update

---
+++ Assets/Haunted URP Tools/Scripts/Dithering/Dithering.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace PSX
+{
+    public class Dithering : VolumeComponent, IPostProcessComponent
+    {
+        //PIXELATION
+        //public TextureParameter ditherTexture;
+        public IntParameter patternIndex = new IntParameter(0);
+        public FloatParameter ditherThreshold = new FloatParameter(512);
+        public FloatParameter ditherStrength = new FloatParameter(1);
+        public FloatParameter ditherScale = new FloatParameter(2);
+
+        public ClampedIntParameter rBitDepth = new ClampedIntParameter(8, 1, 8);
+        public ClampedIntParameter gBitDepth = new ClampedIntParameter(8, 1, 8);
+        public ClampedIntParameter bBitDepth = new ClampedIntParameter(8, 1, 8);
+
+        public ClampedIntParameter ditherSeparation = new ClampedIntParameter(256, 1, 4096);
+
+        public ClampedFloatParameter intensity = new ClampedFloatParameter(0f, 0f, 1f);
+
+        public ClampedIntParameter downscaleFactor = new ClampedIntParameter(4, 1, 8);
+
+
+        //PIXELATION
+        public FloatParameter widthPixelation = new FloatParameter(512);
+        public FloatParameter heightPixelation = new FloatParameter(512);
+
+        //COLOR PRECISION 
+        public FloatParameter colorPrecision = new FloatParameter(32.0f);
+
+        //INTERFACE REQUIREMENT 
+        public bool IsActive() => true;
+        public bool IsTileCompatible() => false;
+    }
+}
